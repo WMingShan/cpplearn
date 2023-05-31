d76cca51e994c6ccc1c5747ff3ae22582c97dfe5
明山 文 <1533028477@qq.com>
2023-05-31 09:08:31 +0800

    message

---
+++ code/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	/* code */
+	short num1 = 10;
+	int num2 = 10;
+	long num3 = 10;
+	cout << num1 << "=" << num2 << "=" << num3 << "=" << endl;
+	return 0;
+}
